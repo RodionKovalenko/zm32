6b771755956757844e0a34c4280ad7f4534353d1
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2024-07-13 19:41:58 +0200

    change to https

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,6 @@ set -e
 /usr/local/bin/wait-for-it database:3306 --
 php bin/console cache:clear --env=prod --no-debug
 php bin/console cache:warmup --env=prod --no-debug
-
-composer install --optimize-autoloader --no-dev
 php bin/console doctrine:migrations:diff --allow-empty-diff --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
