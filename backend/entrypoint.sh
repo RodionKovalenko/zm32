06d7675d67c023048374dfa63ba43db7a0ccf906
Rodion Kovalenko <rodion_kovalenko@yahoo.dem>
2024-07-14 21:56:58 +0200

    git ignore

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -28,7 +28,7 @@ php bin/console cache:clear --env=prod --no-debug
 php bin/console cache:warmup --env=prod --no-debug
 php bin/console doctrine:migrations:diff --allow-empty-diff --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
-#php bin/console zm:generate-default-artikels
+php bin/console zm:generate-default-artikels
 
 # Start PHP-FPM
 exec php-fpm
