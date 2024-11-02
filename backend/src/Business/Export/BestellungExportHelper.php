b1afb62a4bca747f45a6a9479f849be03afc1240
Rodion Kovalenko <rodion_kovalenko@yahoo.dem>
2024-11-02 21:02:43 +0100

    export

--- backend/src/Business/Export/BestellungExportHelper.php
+++ backend/src/Business/Export/BestellungExportHelper.php
@@ -17,15 +17,15 @@ class BestellungExportHelper
         // Create new PDF document
         $pdf = new \TCPDF();
         $currentDateString = (new \DateTime())->format('d.m.Y H:i');
-        $dokumentTitle = 'Export Bestellungen ' . $currentDateString;
+        $dokumentTitle = 'Bestellungen ' . $currentDateString;
 
+        $paddingRight = '                                                                                                                                        ';
         // Set document information
         $pdf->setCreator(PDF_CREATOR);
-        $pdf->setAuthor('ZM 32');
-        $pdf->setTitle($dokumentTitle);
-        $pdf->setSubject('Bestellungen');
+        $pdf->setAuthor('ZM 32'); // This will still be set as Author metadata
 
-        $pdf->setHeaderData('', 0, $dokumentTitle, 'ZM 32', array(0, 64, 255), array(0, 64, 128));
+        // Customize header to display title left-aligned and "ZM 32" right-aligned
+        $pdf->setHeaderData('', 0, $dokumentTitle, $paddingRight . 'ZM 32', array(0, 64, 255), array(0, 64, 128));
         $pdf->setHeaderFont(array('helvetica', '', 12));
         $pdf->setFooterFont(array('helvetica', '', 10));
         $pdf->setMargins(15, 30, 15);
@@ -35,8 +35,17 @@ class BestellungExportHelper
         $pdf->setFont('helvetica', '', 10);
         $pdf->addPage();
 
+        // Calculate remaining width for Artikel and Notizen columns
+        $fixedWidthColumns = 15 + 20 + 20 + 30; // widths of Menge, Bestellnummer, Preis, and Bestellt von
+        $totalPageWidth = 210 - 30; // Total A4 width minus 15mm margins on each side
+        $remainingWidth = $totalPageWidth - $fixedWidthColumns;
+
+        // Divide remaining width between Artikel and Notizen columns
+        $artikelWidth = $remainingWidth * 0.6; // 60% of remaining width
+        $notizenWidth = $remainingWidth * 0.4; // 40% of remaining width
+
         // Initialize content variable
-        $html = '<h1 style="margin-bottom: 5px;">' . $dokumentTitle . '</h1>';
+        $html = '';
 
         // Group Bestellungen by Lieferants
         $bestellungenByLieferant = [];
@@ -60,29 +69,36 @@ class BestellungExportHelper
             // Start the table for this Lieferant group with smaller cell padding
             $html .= '<table border="1" cellpadding="3" cellspacing="0" width="100%" style="border-collapse: collapse;">';
 
-            // Add table header (only once per Lieferant)
+            // Add table header with modified column widths
             $html .= '<thead>
                     <tr>
-                        <th width="30mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Menge</th>
-                        <th width="45mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Bestellnummer</th>
-                        <th width="45mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Artikel</th>
-                        <th width="30mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Notizen</th>
+                        <th width="15mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Menge</th>
+                        <th width="20mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Bestell-<br/>nummer</th>
+                        <th width="' . $artikelWidth . 'mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Artikel</th>
+                        <th width="20mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Preis</th>
+                        <th width="' . $notizenWidth . 'mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Notizen</th>
+                        <th width="30mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Bestellt von</th>
                     </tr>
                   </thead>';
 
             $html .= '<tbody>';
 
             // Data rows for each Bestellung in the current Lieferant group
+            /** @var Bestellung $bestellung */
             foreach ($bestellungen as $bestellung) {
                 /** @var Artikel $artikel */
                 foreach ($bestellung->getArtikels() as $artikel) {
                     $lieferantBestellnummer = $this->getLieferantBestellnummer($bestellung, $bestellung->getLieferants()[0]);
+                    $bestelltVon = $bestellung->getMitarbeiter()->getVorname() . ' ' . $bestellung->getMitarbeiter()->getNachname();
+                    $preis = $bestellung->getPreis() ? $bestellung->getPreis()  . ' €' : '';
 
                     $html .= '<tr>';
-                    $html .= '<td width="30mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestellung->getAmount() . '</td>';
-                    $html .= '<td width="45mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . ($lieferantBestellnummer ?? 'N/A') . '</td>';
-                    $html .= '<td width="45mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $artikel->getName() . '</td>';
-                    $html .= '<td width="30mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestellung->getDescription() . '</td>';
+                    $html .= '<td width="15mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestellung->getAmount() . '</td>';
+                    $html .= '<td width="20mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . ($lieferantBestellnummer ?? 'N/A') . '</td>';
+                    $html .= '<td width="' . $artikelWidth . 'mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $artikel->getName() . '</td>';
+                    $html .= '<td width="20mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $preis . '</td>';
+                    $html .= '<td width="' . $notizenWidth . 'mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestellung->getDescription() . '</td>';
+                    $html .= '<td width="30mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestelltVon . '</td>';
                     $html .= '</tr>';
                 }
             }
@@ -106,7 +122,9 @@ class BestellungExportHelper
         $pdf->Output('export_bestellungen.pdf', 'D');
     }
 
-    private function getLieferantBestellnummer(Bestellung $bestellung, ?Lieferant $lieferant = null)
+
+
+        private function getLieferantBestellnummer(Bestellung $bestellung, ?Lieferant $lieferant = null)
     {
         if ($lieferant === null) {
             return null;
