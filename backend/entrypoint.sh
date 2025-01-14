502708829304071442b07fffdad5607cdbbdef51
Rodion Kovalenko <rodion_kovalenko@yahoo.dem>
2025-01-14 09:27:03 +0100

    update angular 19.0.7

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,19 +9,23 @@ if TABLES=$(mysql -h "$DB_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW TA
     TABLES=$(echo "$TABLES" | sed -n '2,$p')
 
     if [ -z "$TABLES" ]; then
-        echo "No tables found in the database."
-        echo "Database does not exist. Deleting migration files..."
-        rm -rf var/Migrations/*
-        php bin/console cache:clear --env=prod --no-debug
-        php bin/console cache:warmup --env=prod --no-debug
-        php bin/console doctrine:migrations:diff --allow-empty-diff --no-interaction
+        echo "No tables found in the database. Initializing database setup..."
+
+        # Ensure migration files exist and are properly synchronized
+        if [ ! "$(ls -A var/Migrations)" ]; then
+            echo "No migration files found. Generating new migration files..."
+            php bin/console doctrine:migrations:diff --allow-empty-diff --no-interaction
+        fi
+
+        echo "Running migrations to set up the database..."
         php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+
+        echo "Generating default articles..."
         php bin/console zm:generate-default-artikels
     else
-        echo "Tables found in the database. Default execution:"
+        echo "Tables found in the database. Ensuring consistency..."
         echo "$TABLES"
-        php bin/console cache:clear --env=prod --no-debug
-        php bin/console cache:warmup --env=prod --no-debug
+
         php bin/console doctrine:migrations:diff --allow-empty-diff --no-interaction
         php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
     fi
@@ -31,7 +35,13 @@ else
     echo "MYSQL_USER: $MYSQL_USER"
     echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
     echo "MYSQL_DATABASE: $MYSQL_DATABASE"
+    exit 1
 fi
+
+# Clear and warmup the cache
+echo "Clearing and warming up the cache..."
+php bin/console cache:clear --env=prod --no-debug
+php bin/console cache:warmup --env=prod --no-debug
 
 # Start PHP-FPM
 exec php-fpm
