9d8d61daa17eacead3c0217f5271f2bf679645b6
Rodion Kovalenko <rodion_kovalenko@yahoo.dem>
2025-02-09 23:09:03 +0100

    jwt token

--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -1,40 +1,51 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { environment } from '../../enviroments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class HttpService {
-    //baseUrl = 'https://iba-backend.ddev.site/api';
-    private baseUrl = 'https://127.0.0.1:54952/api' ;
-    //baseUrl = 'https://iba.local.de/api';
-    loginURL = `${this.baseUrl}/login`;
+  private baseUrl = 'https://127.0.0.1:62487/api';
+  private loginURL = '';
+  private refreshJWTURL = '';
+  //baseUrl = 'https://iba.local.de/api';
 
-    constructor(private httpClient: HttpClient) {
-        let baselocation = window.location.href.split('/#')[0];
-        baselocation = baselocation.trim();
+  constructor(private httpClient: HttpClient) {
+    let prod = environment.production;
 
-        let lastChar = baselocation.charAt(baselocation.length - 1);
+    if (prod) {
+      let baselocation = window.location.href.split('/#')[0];
+      baselocation = baselocation.trim();
 
-        if (lastChar === '/') {
-            baselocation = baselocation.slice(0, -1);
-        }
+      let lastChar = baselocation.charAt(baselocation.length - 1);
 
-        this.baseUrl = baselocation + '/api';
-        this.loginURL = `${this.baseUrl}/login`;
+      if (lastChar === '/') {
+        baselocation = baselocation.slice(0, -1);
+      }
+      this.baseUrl = baselocation + '/api';
     }
 
-    get_httpclient() {
-        return this.httpClient;
-    }
+    this.loginURL = `${this.baseUrl}/login`;
+    this.refreshJWTURL = `${this.baseUrl}/login/refresh-jwt-token`;
+  }
 
-    get_baseUrl() {
-        return this.baseUrl;
-    }
+  get_httpclient() {
+    return this.httpClient;
+  }
 
-    loginMitarbeiter(mitarbeiterId: Number) {
-        return this.httpClient.get(this.loginURL + '/' + mitarbeiterId);
-    }
+  get_baseUrl() {
+    return this.baseUrl;
+  }
+
+  loginMitarbeiter(mitarbeiterId: Number) {
+    return this.httpClient.get(this.loginURL + '/' + mitarbeiterId);
+  }
+
+  refreshToken() {
+    const refresh_token = localStorage.getItem('refresh-token');  // The API endpoint URL
+    return this.httpClient.post(this.refreshJWTURL, {refresh_token: refresh_token});
+  }
 }
 
 
@@ -43,8 +54,8 @@ import {Observable} from 'rxjs';
 
 @Injectable()
 export class HttpAppInterceptor implements HttpInterceptor {
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // Modify the request or response here
-        return next.handle(request);
-    }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Modify the request or response here
+    return next.handle(request);
+  }
 }
---
+++ frontend/src/enviroments/environment-prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true
+};
+
+export const ibaApp = {
+    user: null
+};
