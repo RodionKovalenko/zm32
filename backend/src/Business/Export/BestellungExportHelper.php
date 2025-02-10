ce8d2adda78ee481a3a7bcfd6e4ccdfa082aa152
Rodion Kovalenko <rodion_kovalenko@yahoo.dem>
2025-02-10 15:18:17 +0100

    export

--- backend/src/Business/Export/BestellungExportHelper.php
+++ backend/src/Business/Export/BestellungExportHelper.php
@@ -19,97 +19,92 @@ class BestellungExportHelper
         $currentDateString = (new \DateTime())->format('d.m.Y H:i');
         $dokumentTitle = 'Bestellungen ' . $currentDateString;
 
-        $paddingRight = '                                                                                                                                        ';
-        // Set document information
         $pdf->setCreator(PDF_CREATOR);
-        $pdf->setAuthor('ZM 32'); // This will still be set as Author metadata
-
-        // Customize header to display title left-aligned and "ZM 32" right-aligned
-        $pdf->setHeaderData('', 0, $dokumentTitle, $paddingRight . 'ZM 32', array(0, 64, 255), array(0, 64, 128));
+        $pdf->setAuthor('ZM 32');
+        $pdf->setHeaderData('', 0, $dokumentTitle, 'ZM 32', array(0, 64, 255), array(0, 64, 128));
         $pdf->setHeaderFont(array('helvetica', '', 12));
         $pdf->setFooterFont(array('helvetica', '', 10));
         $pdf->setMargins(15, 30, 15);
-        $pdf->setHeaderMargin(10);
-        $pdf->setFooterMargin(15);
         $pdf->setAutoPageBreak(true, 20);
         $pdf->setFont('helvetica', '', 10);
         $pdf->addPage();
 
-        // Calculate remaining width for Artikel and Notizen columns
-        $fixedWidthColumns = 15 + 20 + 20 + 30; // widths of Menge, Bestellnummer, Preis, and Bestellt von
-        $totalPageWidth = 210 - 30; // Total A4 width minus 15mm margins on each side
+        // Table column widths
+        $fixedWidthColumns = 15 + 20 + 20 + 30; // Menge, Bestellnummer, Preis, Bestellt von
+        $totalPageWidth = 210 - 30; // A4 width minus margins
         $remainingWidth = $totalPageWidth - $fixedWidthColumns;
+        $artikelWidth = $remainingWidth * 0.6;
+        $notizenWidth = $remainingWidth * 0.4;
 
-        // Divide remaining width between Artikel and Notizen columns
-        $artikelWidth = $remainingWidth * 0.6; // 60% of remaining width
-        $notizenWidth = $remainingWidth * 0.4; // 40% of remaining width
-
-        // Initialize content variable
         $html = '';
-
-        // Group Bestellungen by Lieferants
+        $totalSum = 0;
         $bestellungenByLieferant = [];
 
+        // Group orders by supplier
         foreach ($bestellungen as $bestellung) {
-            /** @var Lieferant $lieferant */
             $lieferant = $bestellung->getLieferants()[0] ?? null;
-
-            if ($lieferant !== null) {
-                $bestellungenByLieferant[$lieferant->getName()][] = $bestellung;
-            } else {
-                $bestellungenByLieferant['N/A'][] = $bestellung;
-            }
+            $lieferantName = $lieferant ? $lieferant->getName() : 'N/A';
+            $bestellungenByLieferant[$lieferantName][] = $bestellung;
         }
 
-        // Loop through each Lieferant group
+        // Generate table for each supplier
         foreach ($bestellungenByLieferant as $lieferantName => $bestellungen) {
-            // Add Lieferant header with smaller font and reduced margin
             $html .= '<h2 style="font-size: 14px; margin: 5px 0;">Lieferant: ' . $lieferantName . '</h2>';
-
-            // Start the table for this Lieferant group with smaller cell padding
             $html .= '<table border="1" cellpadding="3" cellspacing="0" width="100%" style="border-collapse: collapse;">';
 
-            // Add table header with modified column widths
             $html .= '<thead>
                     <tr>
-                        <th width="15mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Menge</th>
-                        <th width="20mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Bestell-<br/>nummer</th>
-                        <th width="' . $artikelWidth . 'mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Artikel</th>
-                        <th width="20mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Preis</th>
-                        <th width="' . $notizenWidth . 'mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Notizen</th>
-                        <th width="30mm" style="border: 1px solid #000; background-color: #f2f2f2; text-align: left;">Bestellt von</th>
+                        <th width="15mm">Menge</th>
+                        <th width="20mm">Bestellnummer</th>
+                        <th width="' . $artikelWidth . 'mm">Artikel</th>
+                        <th width="20mm">Preis</th>
+                        <th width="' . $notizenWidth . 'mm">Notizen</th>
+                        <th width="30mm">Bestellt von</th>
                     </tr>
                   </thead>';
 
             $html .= '<tbody>';
+            $supplierSum = 0;
 
-            // Data rows for each Bestellung in the current Lieferant group
-            /** @var Bestellung $bestellung */
             foreach ($bestellungen as $bestellung) {
-                /** @var Artikel $artikel */
                 foreach ($bestellung->getArtikels() as $artikel) {
                     $lieferantBestellnummer = $this->getLieferantBestellnummer($bestellung, $bestellung->getLieferants()[0]);
                     $bestelltVon = $bestellung->getMitarbeiter()->getVorname() . ' ' . $bestellung->getMitarbeiter()->getNachname();
-                    $preis = $bestellung->getPreis() ? $bestellung->getPreis()  . ' €' : '';
+                    $preis = $bestellung->getPreis() ?: 0;
+                    $preis = (float)str_replace(',', '.', $preis);
+                    $formattedPreis = number_format($preis, 2, ',', '.') . ' €';
 
-                    $html .= '<tr>';
-                    $html .= '<td width="15mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestellung->getAmount() . '</td>';
-                    $html .= '<td width="20mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . ($lieferantBestellnummer ?? 'N/A') . '</td>';
-                    $html .= '<td width="' . $artikelWidth . 'mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $artikel->getName() . '</td>';
-                    $html .= '<td width="20mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $preis . '</td>';
-                    $html .= '<td width="' . $notizenWidth . 'mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestellung->getDescription() . '</td>';
-                    $html .= '<td width="30mm" style="border: 1px solid #000; vertical-align: top; padding: 3px;">' . $bestelltVon . '</td>';
-                    $html .= '</tr>';
+                    $supplierSum += $preis;
+                    $totalSum += $preis;
+
+                    $html .= '<tr>
+                        <td width="15mm">' . $bestellung->getAmount() . '</td>
+                        <td width="20mm">' . ($lieferantBestellnummer ?? 'N/A') . '</td>
+                        <td width="' . $artikelWidth . 'mm">' . $artikel->getName() . '</td>
+                        <td width="20mm" style="text-align: right;">' . $formattedPreis . '</td>
+                        <td width="' . $notizenWidth . 'mm">' . $bestellung->getDescription() . '</td>
+                        <td width="30mm">' . $bestelltVon . '</td>
+                      </tr>';
                 }
             }
 
-            $html .= '</tbody></table>';  // Close the table for this Lieferant group
+            // Add subtotal row
+            $html .= '<tr style="font-weight: bold; background-color: #f2f2f2;">
+                <td colspan="3" style="text-align: right;">Summe für ' . $lieferantName . ':</td>
+                <td style="text-align: right;">' . number_format($supplierSum, 2, ',', '.') . ' €</td>
+                <td colspan="2"></td>
+              </tr>';
+
+            $html .= '</tbody></table><br>';
         }
+
+        // Add final total sum row
+        $html .= '<h2 style="text-align: right; margin-top: 15px;">Gesamtsumme: ' . number_format($totalSum, 2, ',', '.') . ' €</h2>';
 
         // Output the content
         $pdf->writeHTML($html, true, false, true, false, '');
 
-        // Set HTTP headers for PDF inline display or download
+        // Set HTTP headers
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
@@ -118,20 +113,17 @@ class BestellungExportHelper
         header('Content-Transfer-Encoding: binary');
         header('Content-Length: ' . strlen($pdf->Output('', 'S')));
 
-        // Close and output PDF document
+        // Close and output PDF
         $pdf->Output('export_bestellungen.pdf', 'D');
     }
 
-
-
-        private function getLieferantBestellnummer(Bestellung $bestellung, ?Lieferant $lieferant = null)
+    private function getLieferantBestellnummer(Bestellung $bestellung, ?Lieferant $lieferant = null)
     {
         if ($lieferant === null) {
             return null;
         }
-        /** @var Artikel $artikel */
+
         foreach ($bestellung->getArtikels() as $artikel) {
-            /** @var ArtikelToLieferBestellnummer $artikelToLieferantBestellnummer */
             foreach ($artikel->getArtikelToLieferantBestellnummers() as $artikelToLieferantBestellnummer) {
                 if ($artikelToLieferantBestellnummer->getLieferant() === $lieferant) {
                     return $artikelToLieferantBestellnummer->getBestellnummer();
